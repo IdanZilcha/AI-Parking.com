873d306e954bdb10808bc06ed56ef604e08259b2
IdanZilcha <110530875+IdanZilcha@users.noreply.github.com>
2022-10-13 15:21:47 +0300

    Add files via upload

---
+++ assets/js/my_script.js
@@ -0,0 +1,16 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
